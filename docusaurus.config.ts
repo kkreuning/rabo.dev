ed8451577fe4074ce1aa74906b159b678137c9a2
Kay Kreuning <kay.kreuning@ximedes.com>
2024-09-05 10:41:03 +0200

    Add GitHub Action for deployment to GitHub Pages.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,14 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/rabo.dev/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kkreuning', // Usually your GitHub org/user name.
+  projectName: 'rabo.dev', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
